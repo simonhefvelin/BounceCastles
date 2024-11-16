85c7cb2b0d2b98c92a4b7cf40027d103433cfe08
simonhefvelin <simon_nr6@hotmail.com>
2024-11-16 21:03:41 +0100

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace BounceCastles.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
